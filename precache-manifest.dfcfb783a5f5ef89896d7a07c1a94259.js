5e461683fa3f31430818d661ecc4f00ff69a5be2
Parthiban <parthiban.n@navoinc.com>
2020-07-20 16:54:14 +0530

    Updates

---
+++ precache-manifest.dfcfb783a5f5ef89896d7a07c1a94259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d786a9f66f7691b4aadd7813c63ec17",
+    "url": "/Flexi-Config/index.html"
+  },
+  {
+    "revision": "477d73ebef026c14bc52",
+    "url": "/Flexi-Config/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1adbcba2544b9768fc87",
+    "url": "/Flexi-Config/static/css/main.600f4dc5.chunk.css"
+  },
+  {
+    "revision": "477d73ebef026c14bc52",
+    "url": "/Flexi-Config/static/js/2.634e27fd.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/Flexi-Config/static/js/2.634e27fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1adbcba2544b9768fc87",
+    "url": "/Flexi-Config/static/js/main.a0fb2c68.chunk.js"
+  },
+  {
+    "revision": "aaec261017ac58f6081e",
+    "url": "/Flexi-Config/static/js/runtime-main.8d609197.js"
+  }
+]);
